e4b018b084e0d6db7834cffb110ec2eb41b7a03c
Lithiniskhikhi <lithiniskhikhi9@gmail.com>
2023-05-17 10:08:40 +0200

    commit

---
+++ script/code.js
@@ -0,0 +1,23 @@
+// string
+ let name = "Lee";
+ console.log(typeof name);
+
+//number
+// let name = 5 ;
+// console.log(typeof name);
+
+//boolean
+ let selected = true ;
+console.log(typeof selected);
+
+//undefined
+let surname ;
+ console.log(typeof surname);
+
+//null
+let lastName = null
+ console.log(typeof lastName);
+
+//number type
+let numb = 30.5 ;
+console.log(typeof numb);
